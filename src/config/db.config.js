1f2fc539d71179fea4d4d8b7eb64a1fd216bdb35
Nidia Ardila Piñeros <nidiaardila17@gmail.com>
2025-01-11 01:38:50 +0100

    estructua de rutas, creando los endpoints

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -6,5 +6,6 @@ dotenv.config()
 export const dbConfig = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT || "postgres",
+
 });
